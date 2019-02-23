19d323d98351d819088ce449e6028a35c07acbd3
Tomás <tomini18@hotmail.com>
2019-02-23 18:57:10 +0100

    created "Articulo" controller and views

---
+++ ProyectoTienda/Controllers/ArticuloController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoTienda;
+
+namespace ProyectoTienda.Controllers
+{
+    public class ArticuloController : Controller
+    {
+        private TiendaEntities db = new TiendaEntities();
+
+        // GET: Articulo
+        public ActionResult Index()
+        {
+            return View(db.Articulos.ToList());
+        }
+
+        // GET: Articulo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulos.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // GET: Articulo/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Articulo/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,nombre,stock,precio")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Articulos.Add(articulo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(articulo);
+        }
+
+        // GET: Articulo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulos.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articulo/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,nombre,stock,precio")] Articulo articulo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(articulo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(articulo);
+        }
+
+        // GET: Articulo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Articulo articulo = db.Articulos.Find(id);
+            if (articulo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(articulo);
+        }
+
+        // POST: Articulo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Articulo articulo = db.Articulos.Find(id);
+            db.Articulos.Remove(articulo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
